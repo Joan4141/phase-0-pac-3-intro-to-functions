275d55faa8809f01882285562a7f2a0d8139a8a0
Joan <joan.wafula@student.moringaschool.com>
2022-08-05 22:46:29 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Follow along with the examples here
+function doNothing() {}
+function sayHello() {
+    console.log("Hello!");
+  }
+  sayHello();
+  function sayHelloToIsabel() {
+    console.log("Hello, Isabel!");
+  }
+  function sayHelloToSofia() {
+    console.log("Hello, Sofia!");
+  }
+  function sayHelloToBrendan() {
+    console.log("Hello, Brendan!");
+  }
+  sayHelloToIsabel();
+sayHelloToSofia();
+sayHelloToBrendan();
+function say(greeting, firstName) {
+    console.log(`${greeting}, ${firstName}!`);
+  }
+  say("Goodbye", "Julio");
+  say("Julio", "hello");
+  function add(1, 2) {
+    return 1 + 2;
+  }
+  console.log(add(1, 2));
+  
